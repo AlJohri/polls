233cc869c547ca9e5d49167e1c6a8e38c500629b
Al Johri <al.johri@gmail.com>
2015-04-04 22:55:09 -0500

    change cross_origin

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ from flask.ext.pymongo import PyMongo
 from flask.ext.cors import cross_origin
 app = Flask(__name__)
 
-@app.after_request
-@cross_origin("*") # allow all origins all methods everywhere in the app
-def after(response):
-	return response
+# @app.after_request
+# @cross_origin("*") # allow all origins all methods everywhere in the app
+# def after(response):
+# 	return response
 
 @app.route("/rcp")
+@cross_origin("*")
 def rcp():
     t = threading.Thread(name="rcp", target=RCPCurrent.download, args=())
     t.start()
     return "started"
 
 @app.route("/hp")
+@cross_origin("*")
 def hp():
     t = threading.Thread(name="hp", target=HPCurrent.download, args=())
     t.start()
